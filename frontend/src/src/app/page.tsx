f51dba475beab6fd6697070f5327da8782bdccc6
Shinya0714 <doradoranobinobi93871293@gmail.com>
2024-09-11 11:15:16 +0900

    refactor:デザイン調整

--- frontend/src/src/app/page.tsx
+++ frontend/src/src/app/page.tsx
@@ -127,184 +127,38 @@ export default function Home() {
   <>
   <div className="bg-white py-6 sm:py-8 lg:py-12">
     <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-      {/* <!-- text - start --> */}
       <div className="mb-10 md:mb-16">
         <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">fyfy</h2>
-
-        <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">
+        <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg mb-10">
           ユーザーがファイルをアップロードし、特定の人と安全に共有できるプラットフォーム。<br/>
           ファイルは暗号化され、アクセス制御が厳密に管理される。
         </p>
-        
-        <input type="file" onChange={fileSelect} />
-        <button type="submit" onClick={upload}>Upload</button>
-        <ul>
-          {items.map((item, index) => (
-            <li key={index}>
-              <strong>ID:</strong> {item.ID.S} <br />
-              <strong>Name:</strong> {item.Name.S} <br />
-              <button onClick={(event) => download(event, item.ID.S)}>Download</button>
-            </li>
-          ))}
-        </ul>
+        <div  className="mx-auto max-w-screen-md text-center md:text-lg">
+          <input className="mb-10 block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" onChange={fileSelect} />
+          <button type="submit" onClick={upload} className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
+            Upload
+          </button>
+        </div>
       </div>
-      {/* <!-- text - end --> */}
-
       <div className="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-8">
-        {/* <!-- article - start --> */}
-        <div className="flex flex-col overflow-hidden rounded-lg border bg-white">
-          <a href="#" className="group relative block h-48 overflow-hidden bg-gray-100 md:h-64">
-            <img src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Minh Pham" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-          </a>
-
-          <div className="flex flex-1 flex-col p-4 sm:p-6">
-            <h2 className="mb-2 text-lg font-semibold text-gray-800">
-              <a href="#" className="transition duration-100 hover:text-indigo-500 active:text-indigo-600">New trends in Tech</a>
-            </h2>
-
-            <p className="mb-8 text-gray-500">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text.</p>
-
-            <div className="mt-auto flex items-end justify-between">
-              <div className="flex items-center gap-2">
-                <div className="h-10 w-10 shrink-0 overflow-hidden rounded-full bg-gray-100">
-                  <img src="https://images.unsplash.com/photo-1611898872015-0571a9e38375?auto=format&q=75&fit=crop&w=64" loading="lazy" alt="Photo by Brock Wegner" className="h-full w-full object-cover object-center" />
-                </div>
-
-                <div>
-                  <span className="block text-indigo-500">Mike Lane</span>
-                  <span className="block text-sm text-gray-400">July 19, 2021</span>
-                </div>
+        {items.map((item, index) => (
+          <div className="flex flex-col overflow-hidden rounded-lg border bg-white">
+            <a href="#" className="group relative block h-48 overflow-hidden bg-gray-100 md:h-64">
+              <img src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Minh Pham" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+            </a>
+            <div className="flex flex-1 flex-col p-4 sm:p-6">
+              <h2 className="mb-2 text-lg font-semibold text-gray-800">
+                <a href="#" className="transition duration-100 hover:text-indigo-500 active:text-indigo-600">{item.Name.S}</a>
+              </h2>
+              <p className="mb-8 text-gray-500">{item.ID.S}</p>
+              <div className="mt-auto flex items-end justify-between">
+                <button onClick={(event) => download(event, item.ID.S)} className='py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'>
+                  Download
+                </button>
               </div>
-
-              <span className="rounded border px-2 py-1 text-sm text-gray-500">Article</span>
             </div>
           </div>
-        </div>
-        {/* <!-- article - end --> */}
-
-        {/* <!-- article - start --> */}
-        <div className="flex flex-col overflow-hidden rounded-lg border bg-white">
-          <a href="#" className="group relative block h-48 overflow-hidden bg-gray-100 md:h-64">
-            <img src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Lorenzo Herrera" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-          </a>
-
-          <div className="flex flex-1 flex-col p-4 sm:p-6">
-            <h2 className="mb-2 text-lg font-semibold text-gray-800">
-              <a href="#" className="transition duration-100 hover:text-indigo-500 active:text-indigo-600">Working with legacy stacks</a>
-            </h2>
-
-            <p className="mb-8 text-gray-500">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text.</p>
-
-            <div className="mt-auto flex items-end justify-between">
-              <div className="flex items-center gap-2">
-                <div className="h-10 w-10 shrink-0 overflow-hidden rounded-full bg-gray-100">
-                  <img src="https://images.unsplash.com/photo-1586116104126-7b8e839d5b8c?auto=format&q=75&fit=crop&w=64" loading="lazy" alt="Photo by peter bucks" className="h-full w-full object-cover object-center" />
-                </div>
-
-                <div>
-                  <span className="block text-indigo-500">Jane Jackobs</span>
-                  <span className="block text-sm text-gray-400">April 07, 2021</span>
-                </div>
-              </div>
-
-              <span className="rounded border px-2 py-1 text-sm text-gray-500">Article</span>
-            </div>
-          </div>
-        </div>
-        {/* <!-- article - end --> */}
-
-        {/* <!-- article - start --> */}
-        <div className="flex flex-col overflow-hidden rounded-lg border bg-white">
-          <a href="#" className="group relative block h-48 overflow-hidden bg-gray-100 md:h-64">
-            <img src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Magicle" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-          </a>
-
-          <div className="flex flex-1 flex-col p-4 sm:p-6">
-            <h2 className="mb-2 text-lg font-semibold text-gray-800">
-              <a href="#" className="transition duration-100 hover:text-indigo-500 active:text-indigo-600">10 best smartphones for devs</a>
-            </h2>
-
-            <p className="mb-8 text-gray-500">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text.</p>
-
-            <div className="mt-auto flex items-end justify-between">
-              <div className="flex items-center gap-2">
-                <div className="h-10 w-10 shrink-0 overflow-hidden rounded-full bg-gray-100">
-                  <img src="https://images.unsplash.com/photo-1592660503155-7599a37f06a6?auto=format&q=75&fit=crop&w=64" loading="lazy" alt="Photo by Jassir Jonis" className="h-full w-full object-cover object-center" />
-                </div>
-
-                <div>
-                  <span className="block text-indigo-500">Tylor Grey</span>
-                  <span className="block text-sm text-gray-400">March 15, 2021</span>
-                </div>
-              </div>
-
-              <span className="rounded border px-2 py-1 text-sm text-gray-500">Article</span>
-            </div>
-          </div>
-        </div>
-        {/* <!-- article - end --> */}
-
-        {/* <!-- article - start --> */}
-        <div className="flex flex-col overflow-hidden rounded-lg border bg-white">
-          <a href="#" className="group relative block h-48 overflow-hidden bg-gray-100 md:h-64">
-            <img src="https://images.unsplash.com/photo-1610465299996-30f240ac2b1c?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Martin Sanchez" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-          </a>
-
-          <div className="flex flex-1 flex-col p-4 sm:p-6">
-            <h2 className="mb-2 text-lg font-semibold text-gray-800">
-              <a href="#" className="transition duration-100 hover:text-indigo-500 active:text-indigo-600">8 High performance Notebooks</a>
-            </h2>
-
-            <p className="mb-8 text-gray-500">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text.</p>
-
-            <div className="mt-auto flex items-end justify-between">
-              <div className="flex items-center gap-2">
-                <div className="h-10 w-10 shrink-0 overflow-hidden rounded-full bg-gray-100">
-                  <img src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?auto=format&q=75&fit=crop&w=64" loading="lazy" alt="Photo by Aiony Haust" className="h-full w-full object-cover object-center" />
-                </div>
-
-                <div>
-                  <span className="block text-indigo-500">Ann Park</span>
-                  <span className="block text-sm text-gray-400">January 27, 2021</span>
-                </div>
-              </div>
-
-              <span className="rounded border px-2 py-1 text-sm text-gray-500">Article</span>
-            </div>
-          </div>
-        </div>
-        {/* <!-- article - end --> */}
-
-        {/* <!-- article - start --> */}
-        <div className="flex flex-col overflow-hidden rounded-lg border bg-white">
-          <a href="#" className="group relative block h-48 overflow-hidden bg-gray-100 md:h-64">
-            <img src="https://images.unsplash.com/photo-1610465299996-30f240ac2b1c?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Martin Sanchez" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-          </a>
-
-          <div className="flex flex-1 flex-col p-4 sm:p-6">
-            <h2 className="mb-2 text-lg font-semibold text-gray-800">
-              <a href="#" className="transition duration-100 hover:text-indigo-500 active:text-indigo-600">8 High performance Notebooks</a>
-            </h2>
-
-            <p className="mb-8 text-gray-500">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text.</p>
-
-            <div className="mt-auto flex items-end justify-between">
-              <div className="flex items-center gap-2">
-                <div className="h-10 w-10 shrink-0 overflow-hidden rounded-full bg-gray-100">
-                  <img src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?auto=format&q=75&fit=crop&w=64" loading="lazy" alt="Photo by Aiony Haust" className="h-full w-full object-cover object-center" />
-                </div>
-
-                <div>
-                  <span className="block text-indigo-500">Ann Park</span>
-                  <span className="block text-sm text-gray-400">January 27, 2021</span>
-                </div>
-              </div>
-
-              <span className="rounded border px-2 py-1 text-sm text-gray-500">Article</span>
-            </div>
-          </div>
-        </div>
-        {/* <!-- article - end --> */}
+        ))}
       </div>
     </div>
   </div>
